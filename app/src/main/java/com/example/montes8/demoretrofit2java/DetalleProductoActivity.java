70227300caada4cf78ce79edb8da0ec9f74f7070
montes8 <emontesinos812@gmail.com>
2018-07-06 15:57:06 -0500

    creando activitys necesarios

---
+++ app/src/main/java/com/example/montes8/demoretrofit2java/DetalleProductoActivity.java
@@ -0,0 +1,13 @@
+package com.example.montes8.demoretrofit2java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_producto);
+    }
+}
---
+++ app/src/main/java/com/example/montes8/demoretrofit2java/FormularioProductoActivity.java
@@ -0,0 +1,13 @@
+package com.example.montes8.demoretrofit2java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_producto);
+    }
+}
