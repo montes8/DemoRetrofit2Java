7ecc035bbfd049b3d46334220582f88d53621495
montes8 <emontesinos812@gmail.com>
2018-07-06 18:32:39 -0500

    cargando lista en el recicleyview

--- app/src/main/java/com/example/montes8/demoretrofit2java/view/HomeActivity.java
+++ app/src/main/java/com/example/montes8/demoretrofit2java/view/HomeActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import com.example.montes8.demoretrofit2java.R;
 import com.example.montes8.demoretrofit2java.adapter.ListaproductosAdapter;
+import com.example.montes8.demoretrofit2java.api.ProductoService;
+import com.example.montes8.demoretrofit2java.api.RetrofitCreator;
 import com.example.montes8.demoretrofit2java.model.Producto;
-
 import java.util.ArrayList;
+import retrofit2.Call;
+import retrofit2.Retrofit;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -28,6 +30,12 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(mAdapter);
 
+
+        Retrofit retrofit = RetrofitCreator.getInstance();
+        ProductoService service=retrofit.create(ProductoService.class);
+        Call<ArrayList<Producto>> listaProdcutoCallback = service.optenerProductosLista();
+
+        listaProdcutoCallback.enqueue(new Callbac<Producto>() );
 
         mAdapter.addList();
     }
