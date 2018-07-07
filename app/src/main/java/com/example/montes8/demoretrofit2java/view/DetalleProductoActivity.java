f1ff43a98b667b697e8a1adac1a11151d96bb8c2
montes8 <emontesinos812@gmail.com>
2018-07-07 12:16:17 -0500

    implementando toolbar detalle producto

--- app/src/main/java/com/example/montes8/demoretrofit2java/view/DetalleProductoActivity.java
+++ app/src/main/java/com/example/montes8/demoretrofit2java/view/DetalleProductoActivity.java
@@ -2,6 +2,8 @@ package com.example.montes8.demoretrofit2java.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,10 +15,12 @@ public class DetalleProductoActivity extends AppCompatActivity {
     TextView nombredetalle,preciodetalle,lotedetalle,stockdetalle,descripciondetalle;
     Button   actulizar,eliminar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_producto);
+        ajustandoToolbarDetalle();
 
         nombredetalle = findViewById(R.id.nombre_producto_detalle);
         preciodetalle = findViewById(R.id.precio_producto_detalle);
@@ -25,7 +29,6 @@ public class DetalleProductoActivity extends AppCompatActivity {
         descripciondetalle = findViewById(R.id.detalle_producto_detalle);
 
         Bundle extras = getIntent().getExtras();
-
         Producto datosProducto = extras.getParcelable("detalleproducto");
 
         nombredetalle.setText(datosProducto.getNombre());
@@ -34,6 +37,22 @@ public class DetalleProductoActivity extends AppCompatActivity {
         stockdetalle.setText(String.valueOf(datosProducto.getStock()));
         descripciondetalle.setText(datosProducto.getDescripcion());
 
+
+
+
+    }
+
+    public void ajustandoToolbarDetalle(){
+        Toolbar toolbardetalle = (Toolbar) findViewById(R.id.detalleToolbar);
+        setSupportActionBar(toolbardetalle);
+        getSupportActionBar().setTitle("Detalle de Producto");
+        toolbardetalle.setNavigationIcon(R.drawable.ic_atras);
+        toolbardetalle.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
     }
 }
