fa7a801730cc76a5e400e63a578db977025f4778
montes8 <emontesinos812@gmail.com>
2018-07-07 12:08:02 -0500

    pasando datos de producto a la vista detalle producto

--- app/src/main/java/com/example/montes8/demoretrofit2java/view/DetalleProductoActivity.java
+++ app/src/main/java/com/example/montes8/demoretrofit2java/view/DetalleProductoActivity.java
@@ -2,14 +2,38 @@ package com.example.montes8.demoretrofit2java.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.montes8.demoretrofit2java.R;
+import com.example.montes8.demoretrofit2java.model.Producto;
 
 public class DetalleProductoActivity extends AppCompatActivity {
+
+    TextView nombredetalle,preciodetalle,lotedetalle,stockdetalle,descripciondetalle;
+    Button   actulizar,eliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_producto);
+
+        nombredetalle = findViewById(R.id.nombre_producto_detalle);
+        preciodetalle = findViewById(R.id.precio_producto_detalle);
+        lotedetalle = findViewById(R.id.lote_producto_detalle);
+        stockdetalle = findViewById(R.id.stock_detalle_producto);
+        descripciondetalle = findViewById(R.id.detalle_producto_detalle);
+
+        Bundle extras = getIntent().getExtras();
+
+        Producto datosProducto = extras.getParcelable("detalleproducto");
+
+        nombredetalle.setText(datosProducto.getNombre());
+        preciodetalle.setText(String.valueOf(datosProducto.getPrecio()));
+        lotedetalle.setText(String.valueOf(datosProducto.getLote()));
+        stockdetalle.setText(String.valueOf(datosProducto.getStock()));
+        descripciondetalle.setText(datosProducto.getDescripcion());
+
+
     }
 }
